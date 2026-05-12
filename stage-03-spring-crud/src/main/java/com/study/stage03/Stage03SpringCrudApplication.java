dbe25e830b2f2d3483dbe0ad51f30f1358e2d927
sangyun-hwang <s_infinite@naver.com>
2026-05-13 00:07:33 +0900

    [stage-03] Spring Boot 프로젝트와 헬스 체크 API 추가

---
+++ stage-03-spring-crud/src/main/java/com/study/stage03/Stage03SpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.study.stage03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stage03SpringCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stage03SpringCrudApplication.class, args);
+    }
+
+}
