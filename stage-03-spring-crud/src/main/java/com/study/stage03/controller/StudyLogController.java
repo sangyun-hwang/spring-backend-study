cca8a9c32a0a1a121228805edacc61b29f41df76
sangyun-hwang <s_infinite@naver.com>
2026-05-13 17:58:11 +0900

    [stage-03] 카테고리별 StudyLog 조회 추가

--- stage-03-spring-crud/src/main/java/com/study/stage03/controller/StudyLogController.java
+++ stage-03-spring-crud/src/main/java/com/study/stage03/controller/StudyLogController.java
@@ -3,12 +3,36 @@ package com.study.stage03.controller;
 import com.study.stage03.domain.StudyCategory;
 import com.study.stage03.domain.StudyLog;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudyLogController {
+    private final List<StudyLog> logs = new ArrayList<>(List.of(
+            new StudyLog(
+                    1L,
+                    "Java class practice",
+                    StudyCategory.JAVA,
+                    60,
+                    "field and constructor"),
+            new StudyLog(
+                    2L,
+                    "Java array practice",
+                    StudyCategory.JAVA,
+                    40,
+                    "array basics"),
+            new StudyLog(
+                    3L,
+                    "Spring controller practice",
+                    StudyCategory.SPRING,
+                    50,
+                    "RestController and GetMapping")
+    ));
+
+
     @GetMapping("/study-logs/sample")
     public StudyLog getStudyLogSample() {
         return new StudyLog(
@@ -20,26 +44,19 @@ public class StudyLogController {
     }
 
     @GetMapping("/study-logs")
-    public List<StudyLog> getStudyLogs() {
-        return List.of(
-                new StudyLog(
-                        1L,
-                        "Java class practice",
-                        StudyCategory.JAVA,
-                        60,
-                        "field and constructor"),
-                new StudyLog(
-                        2L,
-                        "Java array practice",
-                        StudyCategory.JAVA,
-                        40,
-                        "array basics"),
-                new StudyLog(
-                        3L,
-                        "Spring controller practice",
-                        StudyCategory.SPRING,
-                        50,
-                        "RestController and GetMapping")
-        );
+    public List<StudyLog> getStudyLogs(@RequestParam(required = false) StudyCategory category) {
+        if (category == null) {
+            return logs;
+        }
+        
+        List<StudyLog> filteredLogs = new ArrayList<>();
+
+        for (StudyLog log : logs) {
+            if (log.getCategory().equals(category)) {
+                filteredLogs.add(log);
+            }
+        }
+
+        return filteredLogs;
     }
 }
