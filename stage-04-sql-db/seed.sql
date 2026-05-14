ef7f9ee6df819be07c513f950fcd26bb44456f62
sangyun-hwang <s_infinite@naver.com>
2026-05-14 23:04:50 +0900

    [stage-04] StudyLog 샘플 데이터 INSERT 작성

---
+++ stage-04-sql-db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO study_logs (id, title, category, minutes, memo)
+VALUES (1, 'Java class practice', 'JAVA', 60, 'field and constructor');
+
+INSERT INTO study_logs (id, title, category, minutes, memo)
+VALUES (2, 'Java array practice', 'JAVA', 40, 'array basics');
+
+INSERT INTO study_logs (id, title, category, minutes, memo)
+VALUES (3, 'Spring controller practice', 'SPRING', 50, 'RestController and GetMapping');
