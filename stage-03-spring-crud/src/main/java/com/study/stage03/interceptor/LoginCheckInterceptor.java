a77b4c390be82abdb3db685a80fdf8912369075a
sangyun-hwang <s_infinite@naver.com>
2026-05-28 01:14:51 +0900

    [stage-08] 로그인 체크 인터셉터 추가

---
+++ stage-03-spring-crud/src/main/java/com/study/stage03/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,24 @@
+package com.study.stage03.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler
+    ) throws Exception {
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("loginUser") == null) {
+            response.sendRedirect("/mvc/login");
+            return false;
+        }
+
+        return true;
+    }
+}
