5eb67163ad2209ed6d66b59d7a9f70d09b5340a8
sangyun-hwang <s_infinite@naver.com>
2026-05-28 01:21:19 +0900

    [stage-08] 로그인 체크 인터셉터 등록

---
+++ stage-03-spring-crud/src/main/java/com/study/stage03/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.study.stage03.config;
+
+import com.study.stage03.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/mvc/study-logs/**");
+    }
+}
