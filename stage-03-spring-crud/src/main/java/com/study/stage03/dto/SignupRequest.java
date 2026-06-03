27136862ffbef61e5e37a4ef24dc81f617ea9e21
sangyun-hwang <s_infinite@naver.com>
2026-06-03 16:56:25 +0900

    [stage-11] 회원가입 입력 길이 검증 추가

--- stage-03-spring-crud/src/main/java/com/study/stage03/dto/SignupRequest.java
+++ stage-03-spring-crud/src/main/java/com/study/stage03/dto/SignupRequest.java
@@ -1,12 +1,15 @@
 package com.study.stage03.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class SignupRequest {
     @NotBlank(message = "username is required")
+    @Size(min = 3, max = 20, message = "username must be between 3 and 20 characters")
     private String username;
 
     @NotBlank(message = "password is required")
+    @Size(min = 4, max = 30, message = "password must be between 4 and 30 characters")
     private String password;
 
     public SignupRequest() {
